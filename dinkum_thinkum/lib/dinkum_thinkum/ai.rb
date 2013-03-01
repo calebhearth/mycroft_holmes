2b96505d1c2e2c7aaf20ddb3027fdde14d816dcc
Caleb Thompson <cjaysson@gmail.com>
2013-03-01 15:28:26 -0500

    Add DinkumThinkum::AI
    This is the minimum interface for MycroftHolmes AIs

---
+++ dinkum_thinkum/lib/dinkum_thinkum/ai.rb
@@ -0,0 +1,55 @@
+module DinkumThinkum
+  class AI
+    def onStart
+    end
+
+    def onEnd(is_winner)
+    end
+
+    def onFrame
+    end
+
+    def onSendText(text)
+    end
+
+    def onReceiveText(player, text)
+    end
+
+    def onPlayerLeft(player)
+    end
+
+    def onNukeDetect(target_position)
+    end
+
+    def onUnitDiscover(unit)
+    end
+
+    def onUnitEvade(unit)
+    end
+
+    def onUnitShow(unit)
+    end
+
+    def onUnitHide(unit)
+    end
+
+    def onUnitCreate(unit)
+    end
+
+    def onUnitDestroy(unit)
+    end
+
+    def onUnitMorph(unit)
+    end
+
+    def onUnitRenegade(unit)
+    end
+
+    def onSaveGame(gameName)
+    end
+
+    def onUnitComplete(unit)
+    end
+  end
+end
+
