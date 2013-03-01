f0f2cf9fb7a22ecb9e57525c821f4116a9aa80b6
Caleb Thompson <cjaysson@gmail.com>
2013-03-01 16:02:04 -0500

    snake case DinkumThinkum::AI callback methods

--- dinkum_thinkum/lib/dinkum_thinkum/ai.rb
+++ dinkum_thinkum/lib/dinkum_thinkum/ai.rb
@@ -1,54 +1,54 @@
 module DinkumThinkum
   class AI
-    def onStart
+    def on_start
     end
 
-    def onEnd(is_winner)
+    def on_end(is_winner)
     end
 
-    def onFrame
+    def on_frame
     end
 
-    def onSendText(text)
+    def on_send_text(text)
     end
 
-    def onReceiveText(player, text)
+    def on_receive_text(player, text)
     end
 
-    def onPlayerLeft(player)
+    def on_player_left(player)
     end
 
-    def onNukeDetect(target_position)
+    def on_nuke_detect(target_position)
     end
 
-    def onUnitDiscover(unit)
+    def on_unit_discover(unit)
     end
 
-    def onUnitEvade(unit)
+    def on_unit_evade(unit)
     end
 
-    def onUnitShow(unit)
+    def on_unit_show(unit)
     end
 
-    def onUnitHide(unit)
+    def on_unit_hide(unit)
     end
 
-    def onUnitCreate(unit)
+    def on_unit_create(unit)
     end
 
-    def onUnitDestroy(unit)
+    def on_unit_destroy(unit)
     end
 
-    def onUnitMorph(unit)
+    def on_unit_morph(unit)
     end
 
-    def onUnitRenegade(unit)
+    def on_unit_renegade(unit)
     end
 
-    def onSaveGame(gameName)
+    def on_save_game(gameName)
     end
 
-    def onUnitComplete(unit)
+    def on_unit_complete(unit)
     end
   end
 end
